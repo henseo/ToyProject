bea53b111d9480254598e523509cfe39167a8648
Hyeonseo Jeong <jhyeonseo0523@gmail.com>
2023-05-21 19:13:08 +0900

    Modify guestbook/models.py

--- guestbook/models.py
+++ guestbook/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(verbose_name="작성일시", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="수정일시", auto_now=True)
 
-class GuestBook(models.Model):
+    class Meta:
+        abstract = True
+
+class GuestBook(BaseModel):
     id = models.AutoField(primary_key=True)
     name = models.CharField(verbose_name='작성자', max_length=50)
     message = models.TextField(verbose_name='메시지')
