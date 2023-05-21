c1d143665cedf00c31fd4570e10f89b3ab4da47f
Hyeonseo Jeong <jhyeonseo0523@gmail.com>
2023-05-22 01:56:54 +0900

    Modify config/settings.py/CORS_ALLOWED_ORIGINS

--- config/settings.py
+++ config/settings.py
@@ -84,6 +84,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5500",
     "http://127.0.0.1:5500",    
     "http://likeliontoyproj.kro.kr",
+    "guestbook-toyproject.kro.kr",
 ]
 
 ROOT_URLCONF = 'config.urls'
