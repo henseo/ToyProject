87d5fd6cc012d4a7e40f7292cca7326b5cfb7af7
Hyeonseo Jeong <jhyeonseo0523@gmail.com>
2023-05-21 20:11:49 +0900

    Modify config/settings.py/CORS_ALLOWED_ORIGINS

--- config/settings.py
+++ config/settings.py
@@ -81,6 +81,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",    
     "http://likeliontoyproj.kro.kr:8000",
 ]
 
